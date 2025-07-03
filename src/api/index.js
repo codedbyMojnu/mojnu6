468c907cf9bde5a48f9587f45162f1a810d4153f
codebymojnu <thisismojnu@gmail.com>
2025-07-03 19:52:52 +0600

    change server

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://brain-test-server.onrender.com/",
 });
 
 // Leaderboard API functions
